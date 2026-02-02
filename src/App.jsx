8d10529f311d517aef6adb6edc5bc0a4773a011b
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-02 14:04:41 +0100

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import ButtonComponent from "./components/ButtonComponent"
+import ImageComponent from "./components/ImageComponent"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return ( 
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ButtonComponent title="Hello Babe!" />
+      <ButtonComponent title="Hello Babe!" />
+      <ImageComponent url="https://placedog.net/200/300" altText="Cute dog" />
+      <ImageComponent url="https://placedog.net/300/300" altText="Cute dog" />
+      <ImageComponent url="https://placedog.net/300/250" altText="Cute dog" />
+
     </>
   )
 }
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,5 @@
+const ButtonComponent = function (props) {
+  return <button>{props.title}</button>
+}
+
+export default ButtonComponent
